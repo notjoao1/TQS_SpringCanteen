09b8110a4648dbf7438f5d9afbdb801907a18cc5
Zé Mendes <mendes.j@ua.pt>
2024-04-24 15:48:11 +0100

    Added generated Springboot backend

---
+++ backend/springcanteen/src/main/java/pt/ua/deti/springcanteen/SpringcanteenApplication.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.springcanteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcanteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcanteenApplication.class, args);
+	}
+
+}
---
+++ backend/springcanteen/src/test/java/pt/ua/deti/springcanteen/SpringcanteenApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.springcanteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcanteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
