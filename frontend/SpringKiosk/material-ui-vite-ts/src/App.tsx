f835df3412d20968b26d2d9424469cf092b3e413
notjoao1 <joao.dourado1@ua.pt>
2024-05-07 16:16:23 +0100

    prototype: added sign in and sign up pages

--- frontend/SpringKiosk/material-ui-vite-ts/src/App.tsx
+++ frontend/SpringKiosk/material-ui-vite-ts/src/App.tsx
@@ -8,6 +8,8 @@ import { CssBaseline, PaletteMode } from '@mui/material';
 import Order from './pages/Order';
 import OrderCustomize from './pages/OrderCustomize';
 import OrderFinish from './pages/OrderFinish';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 export default function App() {
   const [mode, setMode] = React.useState<PaletteMode>('light');
@@ -21,12 +23,15 @@ export default function App() {
       <TopBar mode={mode} />
       <BrowserRouter>
         <Routes>
-          <Route path="/" Component={Home}/>
-          <Route path="/order" Component={Order}/>
-          <Route path="/order/customize" Component={OrderCustomize}/>
-          <Route path="/order/finish" Component={OrderFinish}/>
-          <Route path="/restaurants" Component={Home}/>
-          <Route path="/menu" Component={Home}/>
+          <Route path="/" Component={Home} />
+          <Route path="/order" Component={Order} />
+          <Route path="/order/customize" Component={OrderCustomize} />
+          <Route path="/order/finish" Component={OrderFinish} />
+          <Route path="/signup" Component={SignUp} />
+          <Route path="/signin" Component={SignIn} />
+          <Route path="/employee/orders" Component={Home} />
+          <Route path="/employee/kitchen" Component={Home} />
+          <Route path="/menu" Component={Home} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
--- frontend/SpringKiosk/material-ui-vite-ts/src/components/TopBar.tsx
+++ frontend/SpringKiosk/material-ui-vite-ts/src/components/TopBar.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import { Link, PaletteMode } from '@mui/material';
-import Box from '@mui/material/Box';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-import MenuItem from '@mui/material/MenuItem';
-import Drawer from '@mui/material/Drawer';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import { Link, PaletteMode } from "@mui/material";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import Drawer from "@mui/material/Drawer";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const logoStyle = {
-  width: '140px',
-  height: 'auto',
-  cursor: 'pointer',
-  padding: '0.5em 1em 0.5em'
+  width: "140px",
+  height: "auto",
+  cursor: "pointer",
+  padding: "0.5em 1em 0.5em",
 };
 
 interface AppAppBarProps {
@@ -49,81 +49,59 @@ function TopBar({ mode }: AppAppBarProps) {
         position="fixed"
         sx={{
           boxShadow: 0,
-          bgcolor: 'transparent',
-          backgroundImage: 'none',
-          mt: 2
+          bgcolor: "transparent",
+          backgroundImage: "none",
+          mt: 2,
         }}
       >
         <Container maxWidth="lg">
           <Toolbar
             variant="regular"
             sx={(theme) => ({
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-between',
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
               flexShrink: 0,
-              borderRadius: '999px',
+              borderRadius: "999px",
               bgcolor:
-                theme.palette.mode === 'light'
-                  ? 'rgba(255, 255, 255, 0.4)'
-                  : 'rgba(0, 0, 0, 0.4)',
-              backdropFilter: 'blur(24px)',
+                theme.palette.mode === "light"
+                  ? "rgba(255, 255, 255, 0.4)"
+                  : "rgba(0, 0, 0, 0.4)",
+              backdropFilter: "blur(24px)",
               maxHeight: 40,
-              border: '1px solid',
-              borderColor: 'divider',
+              border: "1px solid",
+              borderColor: "divider",
               boxShadow:
-                theme.palette.mode === 'light'
+                theme.palette.mode === "light"
                   ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
-                  : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
+                  : "0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)",
             })}
           >
             <Box
               sx={{
                 flexGrow: 1,
-                display: 'flex',
-                alignItems: 'center',
-                ml: '-18px',
+                display: "flex",
+                alignItems: "center",
+                ml: "-18px",
                 px: 0,
               }}
             >
               {/* Logo image */}
               <Link href="/">
                 <img
-                  src={
-                    '/SpringCanteen_Title.png'
-                  }
+                  src={"/SpringCanteen_Title.png"}
                   style={logoStyle}
                   alt="logo of Spring Canteen"
                 />
               </Link>
-              <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+              <Box sx={{ display: { xs: "none", md: "flex" } }}>
                 <Link href="/order">
                   <MenuItem
                     // onClick={() => scrollToSection('features')}
-                    sx={{ py: '6px', px: '12px' }}
+                    sx={{ py: "6px", px: "12px" }}
                   >
                     <Typography variant="body2" color="text.primary">
-                      Order
-                    </Typography>
-                  </MenuItem>
-                </Link>
-                <Link href="/restaurants">
-                  <MenuItem
-                    // onClick={() => scrollToSection('features')}
-                    sx={{ py: '6px', px: '12px' }}
-                  >
-                    <Typography variant="body2" color="text.primary">
-                      Restaurants
-                    </Typography>
-                  </MenuItem>
-                </Link>
-                <Link href="/menu">
-                  <MenuItem
-                    // onClick={() => scrollToSection('features')}
-                    sx={{ py: '6px', px: '12px' }}
-                  >
-                    <Typography variant="body2" color="text.primary">
-                      Menu
+                      Order now
                     </Typography>
                   </MenuItem>
                 </Link>
@@ -131,9 +109,9 @@ function TopBar({ mode }: AppAppBarProps) {
             </Box>
             <Box
               sx={{
-                display: { xs: 'none', md: 'flex' },
+                display: { xs: "none", md: "flex" },
                 gap: 0.5,
-                alignItems: 'center',
+                alignItems: "center",
               }}
             >
               <Button
@@ -141,54 +119,51 @@ function TopBar({ mode }: AppAppBarProps) {
                 variant="text"
                 size="small"
                 component="a"
-                href="/"
                 target="_blank"
               >
-                Sign in
+                <Link href={"/signin"}>Sign in</Link>
               </Button>
               <Button
                 color="primary"
                 variant="contained"
                 size="small"
                 component="a"
-                href="/"
                 target="_blank"
               >
-                Sign up
+                <Link href={"/signup"}>Sign up</Link>
               </Button>
             </Box>
-            <Box sx={{ display: { sm: '', md: 'none' } }}>
+            <Box sx={{ display: { sm: "", md: "none" } }}>
               <Button
                 variant="text"
                 color="primary"
                 aria-label="menu"
                 onClick={toggleDrawer(true)}
-                sx={{ minWidth: '30px', p: '4px' }}
+                sx={{ minWidth: "30px", p: "4px" }}
               >
                 <MenuIcon />
               </Button>
               <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
                 <Box
                   sx={{
-                    minWidth: '60dvw',
+                    minWidth: "60dvw",
                     p: 2,
-                    backgroundColor: 'background.paper',
+                    backgroundColor: "background.paper",
                     flexGrow: 1,
                   }}
                 >
                   <Box
                     sx={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'end',
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "end",
                       flexGrow: 1,
                     }}
-                  >
-                  </Box>
+                  ></Box>
                   <Link href="/order">
                     <MenuItem
                       // onClick={() => scrollToSection('features')}
-                      sx={{ py: '6px', px: '12px' }}
+                      sx={{ py: "6px", px: "12px" }}
                     >
                       <Typography variant="body2" color="text.primary">
                         Order
@@ -198,7 +173,7 @@ function TopBar({ mode }: AppAppBarProps) {
                   <Link href="/restaurants">
                     <MenuItem
                       // onClick={() => scrollToSection('features')}
-                      sx={{ py: '6px', px: '12px' }}
+                      sx={{ py: "6px", px: "12px" }}
                     >
                       <Typography variant="body2" color="text.primary">
                         Restaurants
@@ -208,7 +183,7 @@ function TopBar({ mode }: AppAppBarProps) {
                   <Link href="/menu">
                     <MenuItem
                       // onClick={() => scrollToSection('features')}
-                      sx={{ py: '6px', px: '12px' }}
+                      sx={{ py: "6px", px: "12px" }}
                     >
                       <Typography variant="body2" color="text.primary">
                         Menu
@@ -223,7 +198,7 @@ function TopBar({ mode }: AppAppBarProps) {
                       component="a"
                       href="/material-ui/getting-started/templates/sign-up/"
                       target="_blank"
-                      sx={{ width: '100%' }}
+                      sx={{ width: "100%" }}
                     >
                       Sign up
                     </Button>
@@ -235,7 +210,7 @@ function TopBar({ mode }: AppAppBarProps) {
                       component="a"
                       href="/material-ui/getting-started/templates/sign-in/"
                       target="_blank"
-                      sx={{ width: '100%' }}
+                      sx={{ width: "100%" }}
                     >
                       Sign in
                     </Button>
@@ -243,7 +218,6 @@ function TopBar({ mode }: AppAppBarProps) {
                 </Box>
               </Drawer>
             </Box>
-
           </Toolbar>
         </Container>
       </AppBar>
