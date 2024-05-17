3ead5155131b417633194aae3ae667739349cace
notjoao1 <joao.dourado1@ua.pt>
2024-05-17 12:04:58 +0100

    SCRUM-24 tests for service work in progress

---
+++ backend/springcanteen/src/test/java/pt/ua/deti/springcanteen/service/PriceServiceTest.java
@@ -0,0 +1,36 @@
+package pt.ua.deti.springcanteen.service;
+
+import org.junit.jupiter.api.Test;
+
+class PriceServiceTest {
+    // focused
+    @Test
+    void whenGetMenuPrice_thenPriceIsCorrectlyCalculated() {
+
+    }
+
+    @Test
+    void whenMenu_hasInvalidMainDish_thenThrowError() {
+
+    }
+
+    @Test
+    void whenMenu_hasInvalidDrink_thenThrowError() {
+
+    }
+
+    @Test
+    void whenMenu_hasInvalidIngredientsForMainDish_thenThrowError() {
+
+    }
+
+    @Test
+    void whenMenu_hasLessIngredientsThanBase_thenPriceShouldNotBeLowerThanBasePrice() {
+
+    }
+
+    @Test
+    void whenMenu_hasMoreIngredientsThanBase_thenPriceShouldBeHigher() {
+
+    }
+}
