89f8d743d95f4f5d45400f7f70e98fd44c545d55
notjoao1 <joao.dourado1@ua.pt>
2024-05-31 15:25:18 +0100

    SCRUM-34 feat: websocket context + hook and small using MenuContext in only required components

--- frontend/SpringKiosk/material-ui-vite-ts/src/App.tsx
+++ frontend/SpringKiosk/material-ui-vite-ts/src/App.tsx
@@ -10,15 +10,16 @@ import OrderCustomize from './pages/OrderCustomize';
 import OrderFinish from './pages/OrderFinish';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
-import EmployeeOrders from './pages/EmployeeOrders';
+import EmployeeCook from './pages/EmployeeCook';
 import EmployeePayments from './pages/EmployeePayments';
 import Customize from './pages/Customize';
 import { NewOrderContextProvider } from './context/NewOrderContext';
-import { MenuContextProvider } from './context/MenuContext';
 import EmployeeReadyToPickUp from './pages/EmployeeReadyToPickUp';
 import { AuthContextProvider } from './context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
 import { EmployeeRole } from './types/EmployeeTypes';
+import MenuContextLayout from './components/MenuContextLayout';
+import { WebSocketProvider } from './context/WebsocketContext';
 
 export default function App() {
   const [mode, setMode] = React.useState<PaletteMode>('light');
@@ -30,33 +31,50 @@ export default function App() {
     <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
       <CssBaseline />
       <AuthContextProvider>
-        <TopBar mode={mode} />
-        <BrowserRouter>
-            <NewOrderContextProvider>
-              <MenuContextProvider>
-                <Routes>
-                  <Route path="/" Component={Home} />
-                  <Route path="/order" Component={Order} />
-                  <Route path="/order/customize" Component={OrderCustomize} />
-                  <Route path="/order/customize/menu/:id" Component={Customize} />
-                  <Route path="/order/finish" Component={OrderFinish} />
-                  <Route path="/signup" Component={SignUp} />
-                  <Route path="/signin" Component={SignIn} />
+          <TopBar mode={mode} />
+          <BrowserRouter>
+              <NewOrderContextProvider>
+                  <Routes>
+                    {/* order pages that require menu data */}
+                    <Route element={<MenuContextLayout/>}>
+                      <Route path="/" Component={Home} />
+                      <Route path="/order" Component={Order} />
+                      <Route path="/order/customize" Component={OrderCustomize} />
+                      <Route path="/order/customize/menu/:id" Component={Customize} />
+                      <Route path="/order/finish" Component={OrderFinish} />
+                    </Route>
+                    
+                    {/* auth pages */}
+                    <Route path="/signup" Component={SignUp} />
+                    <Route path="/signin" Component={SignIn} />
 
-                  <Route element={<PrivateRoute requiredRole={EmployeeRole.COOK}/>} >
-                    <Route path="/employee/orders" Component={EmployeeOrders} />
-                  </Route>
-                  <Route element={<PrivateRoute requiredRole={EmployeeRole.DESK_PAYMENTS}/>} >
-                    <Route path="/employee/payments" Component={EmployeePayments} />
-                  </Route>
-                  <Route element={<PrivateRoute requiredRole={EmployeeRole.DESK_ORDERS}/>} >
-                    <Route path="/employee/ready_orders" Component={EmployeeReadyToPickUp} />
-                  </Route>
+                    {/* employee pages -> require websockets */}
+                    <Route element={
+                      <WebSocketProvider>
+                        <PrivateRoute requiredRole={EmployeeRole.COOK}/>
+                      </WebSocketProvider>
+                    }>
+                      <Route path="/employee/orders" Component={EmployeeCook} />
+                    </Route>
 
-                </Routes>
-              </MenuContextProvider>
-            </NewOrderContextProvider>
-        </BrowserRouter>
+                    <Route element={
+                      <WebSocketProvider>
+                        <PrivateRoute requiredRole={EmployeeRole.DESK_PAYMENTS}/>
+                      </WebSocketProvider>
+                    }>
+                      <Route path="/employee/payments" Component={EmployeePayments}/>
+                    </Route>
+
+                    <Route element={
+                      <WebSocketProvider>
+                        <PrivateRoute requiredRole={EmployeeRole.DESK_ORDERS}/>
+                      </WebSocketProvider>
+                    }>
+                      <Route path="/employee/ready_orders" Component={EmployeeReadyToPickUp}/>
+                    </Route>
+                  </Routes>
+              </NewOrderContextProvider>
+          </BrowserRouter>
       </AuthContextProvider>
     </ThemeProvider>
   );
