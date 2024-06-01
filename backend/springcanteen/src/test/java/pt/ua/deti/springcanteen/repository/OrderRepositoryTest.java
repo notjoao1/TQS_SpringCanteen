cc73f5cd9d5b81664d6e86d796d52c9b3e2778cd
Migas77 <miguel.belchior@ua.pt>
2024-06-01 20:02:15 +0100

    refactor: remove unused imports

--- backend/springcanteen/src/test/java/pt/ua/deti/springcanteen/repository/OrderRepositoryTest.java
+++ backend/springcanteen/src/test/java/pt/ua/deti/springcanteen/repository/OrderRepositoryTest.java
@@ -1,19 +1,13 @@
 package pt.ua.deti.springcanteen.repository;
 
-import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
