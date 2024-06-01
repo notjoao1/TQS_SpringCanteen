a80c09cb71eb469a7e6bea2d11c43d6d93bc8bbd
Zé Mendes <mendes.j@ua.pt>
2024-06-01 22:22:03 +0100

    feat(frontend): Changes appear on refresh, not yet automatically

--- frontend/DigitalSignage/src/pages/DigitalSignage.tsx
+++ frontend/DigitalSignage/src/pages/DigitalSignage.tsx
@@ -1,6 +1,105 @@
 import './DigitalSignage.css'
+import React, { useEffect, useState } from 'react';
+import { useWebSocket } from "../context/WebsocketContext";
+import { CookOrder, OrderStatus, OrderUpdateMessage, WebsocketConnectMessage } from "../types/OrderTypes";
+import { IMessage } from "@stomp/stompjs";
+import { enqueueSnackbar } from "notistack";
 
 export default function DigitalSignage() {
+    const [regularPreparingOrders, setRegularPreparingOrders] = useState<CookOrder[]>([]);
+    const [priorityPreparingOrders, setPriorityPreparingOrders] = useState<CookOrder[]>([]);
+    const [preparingOrders, setPreparingOrders] = useState<CookOrder[]>([]);
+
+    const { websocketClient } = useWebSocket();
+
+    useEffect(() => {
+      websocketClient?.subscribe("/user/topic/orders", (message: IMessage) => {
+        handleReceiveExistingOrders(message);
+      });
+      
+      websocketClient?.subscribe("/topic/orders", (message: IMessage) => {
+        handleReceiveNewOrderOrOrderUpdate(message);
+      })
+    }, [websocketClient])
+  
+    const handleReceiveExistingOrders = (message: IMessage) => {
+      const existingOrders: WebsocketConnectMessage = JSON.parse(message.body);
+      setRegularPreparingOrders(existingOrders.regularPreparingOrders);
+      setPriorityPreparingOrders(existingOrders.priorityPreparingOrders);
+      
+      const allOrders = [...existingOrders.regularPreparingOrders, ...existingOrders.priorityPreparingOrders];
+
+      // order and format the list of orders
+      setPreparingOrders(formatOrderIds(orderList(allOrders)));
+    }
+  
+    const handleReceiveNewOrderOrOrderUpdate = (message: IMessage) => {
+      const receivedMessage: Record<string, any> = JSON.parse(message.body);
+
+      const orderUpdate = receivedMessage as OrderUpdateMessage;
+      const setOldQueue = getOldQueueSetterBasedOnNewStatusAndPriority(orderUpdate.newOrderStatus, orderUpdate.priority);
+      const setNewQueue = getNewQueueSetterBasedOnNewStatusAndPriority(orderUpdate.newOrderStatus, orderUpdate.priority);
+      let cookOrderToMove: CookOrder | undefined = undefined;
+
+
+
+      // remove this order from old queue
+      setOldQueue?.((prevState) => {
+        const updatedQueue = prevState.filter((order) => {
+          if (order.id === orderUpdate.orderId) {
+            cookOrderToMove = order;
+            return false;
+          }
+          return true;
+        });
+        return updatedQueue;
+      })
+
+      // add this order to the new queue
+      setNewQueue?.((prevState) => {
+        return [
+          ...prevState,
+          cookOrderToMove!
+        ]
+      })
+      
+      enqueueSnackbar<"success">(`Successfully updated ${orderUpdate.priority ? "PRIORITY" : ""} order ${orderUpdate.orderId} to ${orderUpdate.newOrderStatus}`, 
+        {variant: "success", autoHideDuration: 5000}
+      );      
+    }
+  
+    // returns old queue setter, so that the order can be removed from the queue
+    // returns undefined if queue is not relevant to this page (cooks don't need to know when orders are picked up, for example)
+    const getOldQueueSetterBasedOnNewStatusAndPriority = (newStatus: OrderStatus, priority: boolean): React.Dispatch<React.SetStateAction<CookOrder[]>> | undefined => {
+      if (newStatus === OrderStatus.READY)
+        return priority ? setPriorityPreparingOrders : setRegularPreparingOrders;
+      return undefined;
+    }
+  
+    // returns new queue, so that the order can be added to the queue
+    // returns undefined if new queue is not relevant to this page (cooks don't need to know when orders are picked up, for example)
+    const getNewQueueSetterBasedOnNewStatusAndPriority = (newStatus: OrderStatus, priority: boolean): React.Dispatch<React.SetStateAction<CookOrder[]>> | undefined => {
+      if (newStatus === OrderStatus.PREPARING)
+        return priority ? setPriorityPreparingOrders : setRegularPreparingOrders;
+      return undefined;
+    }
+
+    // function to order the list of orders by order id, from lowest to highest
+    const orderList = (orders: CookOrder[]) => {
+      return orders.sort((a, b) => a.id - b.id);
+    }
+
+    // function to format the order id to be displayed on the screen
+    // it will display 3 digits, with leading zeros if necessary
+    const formatOrderId = (orderId: number) => {
+      return orderId.toString().padStart(3, '0');
+    }
+
+    // function to format the order ids of the orders to be displayed on the screen
+    const formatOrderIds = (orders: CookOrder[]) => {
+      return orders.map((order) => formatOrderId(order.id));
+    }
+
     return (
         <>
           <div className='container'>
@@ -17,15 +116,10 @@ export default function DigitalSignage() {
             <div className='grid-container orders'>
               <div className="grid-child">
                 <div className='order-container'>
-                  <h2 className="order-child">089</h2>
-                  <h2 className="order-child">090</h2>
-                  <h2 className="order-child">091</h2>
-                  <h2 className="order-child">092</h2>
-                  <h2 className="order-child">093</h2>
-                  <h2 className="order-child">094</h2>
-                  <h2 className="order-child">098</h2>
-                  <h2 className="order-child">099</h2>
-                  <h2 className="order-child">100</h2>
+                  {/* Preparing Orders */}
+                  {preparingOrders.map((orderId) => {
+                    return <h2 key={orderId} className="order-child">{orderId}</h2>
+                  })}
                 </div>
               </div>
               <div className="grid-child">
---
+++ frontend/DigitalSignage/src/types/OrderTypes.tsx
@@ -0,0 +1,82 @@
+import { ICreateMenu, IMenu } from "./MenuTypes";
+
+export interface IOrder {
+    id: number;
+    kiosk_id: number;
+    order_status: OrderStatus;
+    isPaid: boolean;
+    menus: IMenu[];
+    nif: string;
+}
+
+
+export enum OrderStatus {
+    IDLE = "IDLE",
+    PREPARING = "PREPARING",
+    READY = "READY",
+    PICKED_UP = "PICKED_UP",
+    NOT_PAID = "NOT_PAID"
+}
+
+
+// for creating new orders
+export interface ICreateOrder {
+    menus: ICreateMenu[]
+    nif?: string;
+}
+
+export enum PaymentPlace {
+    KIOSK = "In Kiosk",
+    DESK = "In Desk",
+}
+
+
+export interface IOrderResponse {
+    id: number;
+    paid: boolean;
+    priority: boolean;
+    nif: number;
+    orderMenus: IOrderMenuResponse[];
+    price: number;
+}
+
+
+interface IOrderMenuResponse {
+    menu: {name: string, price: number};
+    customization: string;
+}
+
+// only menu data, without customization, see use bellow
+interface CookOrderMenuData {
+    name: string;
+    price: number;
+}
+
+// menu data + customization of that particular menu
+interface CookOrderMenu {
+    menu: CookOrderMenuData;
+    customization: string;
+
+}
+
+
+export interface CookOrder {
+    id: number;
+    priority: boolean;
+    orderMenus: CookOrderMenu[];
+}
+
+export interface WebsocketConnectMessage {
+  regularIdleOrders: CookOrder[];
+  priorityIdleOrders: CookOrder[];
+  regularPreparingOrders: CookOrder[];
+  priorityPreparingOrders: CookOrder[];
+  regularReadyOrders: CookOrder[];
+  priorityReadyOrders: CookOrder[];
+}
+
+export interface OrderUpdateMessage {
+    orderId: number;
+    newOrderStatus: OrderStatus;
+    priority: boolean;
+}
