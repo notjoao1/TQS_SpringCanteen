fc7970ce47326f2635277bc5383eceac4d48d742
notjoao1 <joao.dourado1@ua.pt>
2024-06-01 01:30:37 +0100

    SCRUM-34 fix(frontend): wrong endpoint for websockets

--- frontend/SpringKiosk/material-ui-vite-ts/src/config.ts
+++ frontend/SpringKiosk/material-ui-vite-ts/src/config.ts
@@ -1,5 +1,8 @@
+const VITE_HOST = import.meta.env.VITE_HOST as string ?? "localhost:8080";
+
 const config = {
-    ordersWebSocketUrl: "ws://localhost:8080/websocket",
+    
+    ordersWebSocketUrl: `ws://${VITE_HOST}/websocket`,
 };
   
 export default config;
