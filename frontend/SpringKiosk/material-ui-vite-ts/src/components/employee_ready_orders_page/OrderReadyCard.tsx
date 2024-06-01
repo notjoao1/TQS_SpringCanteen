d0684e824928803e8b1a2015b3b6ce2cb750f369
notjoao1 <joao.dourado1@ua.pt>
2024-06-01 10:56:12 +0100

    SCRUM-34 feat(frontend): receiving orders for the employee confirming orders + small styling changes

--- frontend/SpringKiosk/material-ui-vite-ts/src/components/employee_ready_orders_page/OrderReadyCard.tsx
+++ frontend/SpringKiosk/material-ui-vite-ts/src/components/employee_ready_orders_page/OrderReadyCard.tsx
@@ -1,15 +1,28 @@
-import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, Paper, Typography } from "@mui/material";
+import { CookOrder } from "../../types/OrderTypes";
 
-const OrderReadyCard = () => {
+interface OrderReadyCardProps {
+    order: CookOrder;
+}
+
+const OrderReadyCard = ({order}: OrderReadyCardProps) => {
     return (
         <Card sx={{ minWidth: 275 }}>
             <CardContent>
-                <Typography sx={{ fontSize: 30 }} color="text.secondary" gutterBottom>
-                    ORDER: 10
+                <Paper sx={{ padding: 1, backgroundColor: 'darkgreen', color: 'white', borderRadius: 1, textAlign: 'center', marginBottom: 2 }}>
+                    PRIORITY
+                </Paper>
+                <Typography sx={{ fontSize: 30, fontWeight: 'bold' }} color="text.secondary" gutterBottom>        
+                    ORDER: {order.id}
+                </Typography>
+                <Typography variant="subtitle2">
+                    <span style={{fontWeight: "bold", fontStyle:"italic"}}>Menus: </span>{order.orderMenus.map(o => o.menu.name).join(" ,")}
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Confirm Pick Up</Button>
+                <Button size="small" color="primary">
+                    Confirm Pick Up
+                </Button>
             </CardActions>
         </Card>
     )
